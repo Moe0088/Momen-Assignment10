a81f71c36a047e5e855ad4c71b476631e23a342c
Moe0088 <125282981+Moe0088@users.noreply.github.com>
2024-09-04 11:35:32 -0400

    Project

---
+++ src/main/java/com/coderscampus/Momen_Assignment10/MomenAssignment10Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Momen_Assignment10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomenAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomenAssignment10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/Momen_Assignment10/MomenAssignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Momen_Assignment10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomenAssignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
