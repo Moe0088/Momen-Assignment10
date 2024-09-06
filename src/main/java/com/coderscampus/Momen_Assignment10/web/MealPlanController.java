06425f8e0cd36516f2c4f140d8573b1ac5f7a889
Moe0088 <125282981+Moe0088@users.noreply.github.com>
2024-09-06 11:10:40 -0400

    initial commit

--- src/main/java/com/coderscampus/Momen_Assignment10/web/MealPlanController.java
+++ src/main/java/com/coderscampus/Momen_Assignment10/web/MealPlanController.java
@@ -3,6 +3,7 @@ package com.coderscampus.Momen_Assignment10.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coderscampus.Momen_Assignment10.dto.DayResponse;
@@ -16,13 +17,13 @@ public class MealPlanController {
 	private MealPlanService mealPlanService;
 
 	@GetMapping("mealplanner/week")
-	public ResponseEntity<WeekResponse> getWeekMeals(String numCalories, String diet, String exclusions) {
+	public ResponseEntity<WeekResponse> getWeekMeals(@RequestParam String numCalories, @RequestParam String diet, @RequestParam String exclusions) {
 		return mealPlanService.fetchingDataFromSpoonacular(numCalories, diet, exclusions, "week",
 				WeekResponse.class);
 	}
 
 	@GetMapping("mealplanner/day")
-	public ResponseEntity<DayResponse> getDayMeals(String numCalories, String diet, String exclusions) {
+	public ResponseEntity<DayResponse> getDayMeals( @RequestParam String numCalories, @RequestParam String diet, @RequestParam String exclusions) {
 
 		return mealPlanService.fetchingDataFromSpoonacular(numCalories, diet, exclusions, 
 				"day", DayResponse.class);
